2c137c0c3a043ea8239e165ccf88e7baf957e24d
Yuriy Serbin <serbingeorg@hotmail.com>
2020-03-20 17:50:36 +0200

    next

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ var isFalse = false;
 //other
 var unknown = '1';
 unknown = true;
+//Array
+var numArray1 = [1, 2, 3];
+var numArray2 = [1, 2, 3];
+var strArray1 = ['1', '2', '3'];
+var strArray2 = ['1', '2', '3'];
+var boolArray1 = [true, false];
+var boolArray2 = [true, false];
+//tuples
+var array = [1, 2, '3'];
+var array2 = [true, '3', 2];
--- index.ts
+++ index.ts
@@ -11,3 +11,18 @@ let isFalse: boolean = false;
 //other
 let unknown: string | boolean = '1';
 unknown = true;
+
+//Array
+let numArray1: number[] = [1, 2, 3];
+let numArray2: Array<number> = [1, 2, 3];
+
+let strArray1: string[] = ['1', '2', '3'];
+let strArray2: Array<string> = ['1', '2', '3'];
+
+let boolArray1: boolean[] = [true, false];
+let boolArray2: Array<boolean> = [true, false];
+
+//tuples
+let array: [number, number, string] = [1, 2, '3'];
+
+let array2: [boolean, string, number] = [true, '3', 2];
